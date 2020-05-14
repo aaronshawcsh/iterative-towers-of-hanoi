502ecd5d5e46a5a8e40006dcac668aa7bcfb38f0
Aaron Shaw <aaronshawcsh@gmail.com>
2020-05-14 16:02:55 -0300

    disk movement sequence generation complete, phase 2 in progress

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # iterative-towers-of-hanoi
 an iterative solution to towers of hanoi
 
-this attempt uses a sequence generation algorithm based on the number of disks
+this attempt uses a disk movement sequence generation algorithm based on the number of disks
 
 viability unknown
+
+update 1.01: disk movement sequence generation successful, testing phase complete.
+
+we now know which disk to move at every turn
+
+next step is to figure out which pole to move each disk to
--- src/TowersOfHanoi.java
+++ src/TowersOfHanoi.java
@@ -1,3 +1,35 @@
-public class TowersOfHanoi {
+import java.util.InputMismatchException;
+import java.util.Arrays;
 
+public class TowersOfHanoi {
+    public static void main(String[] args) {
+        testPrintSequence();
+    }
+    public static void testPrintSequence() {
+        int numberOfDisks = 4;
+        System.out.println(Arrays.toString(generateDiskMovementSequence(numberOfDisks)));
+    }
+    public static int[] generateDiskMovementSequence(int numberOfDisks) throws InputMismatchException {
+        if(numberOfDisks < 1) throw new InputMismatchException();
+        else if(numberOfDisks == 1) return new int[] {1};
+        else {
+            int sequenceLength = (int) (Math.pow(2, numberOfDisks) - 1);
+            int[] sequence = new int[sequenceLength];
+            int constant = numberOfDisks, alternate = numberOfDisks - 1, alternateChanger = -1;
+            for(int i = 0; i < sequenceLength / 2; i++) {
+                if(alternate == 2) alternateChanger = 1;
+                else if(alternate == numberOfDisks - 1) alternateChanger = -1;
+                if(i % 2 == 0) {
+                    sequence[i] = constant;
+                    sequence[sequenceLength - 1 - i] = constant;
+                }  else {
+                    sequence[i] = alternate;
+                    sequence[sequenceLength - 1 - i] = alternate;
+                    alternate += alternateChanger;
+                }
+            }
+            sequence[sequenceLength / 2] = 1;
+            return sequence;
+        }
+    }
 }
